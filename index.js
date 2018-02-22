29040e7c1200340c6f42933c1b805a474c9c727a
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-22 21:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file
+function findMatching (list, name) {
+return list.filter(function(name) {
+      return name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  })
+}
